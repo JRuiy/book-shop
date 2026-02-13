541dbf081eea06a8d1a9ca2e384f07af2b903611
vadym <vadym@ohmanalytics.com>
2026-02-13 10:26:13 +0200

    implemented GlobalExceptionHandler and DTO Validation

--- src/main/java/mate/academy/bookshop/controller/BookController.java
+++ src/main/java/mate/academy/bookshop/controller/BookController.java
@@ -1,5 +1,6 @@
 package mate.academy.bookshop.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookshop.dto.BookDto;
@@ -31,7 +32,7 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
@@ -48,7 +49,7 @@ public class BookController {
 
     @PutMapping("/{id}")
     public BookDto updateBookById(@PathVariable Long id,
-                                  @RequestBody UpdateBookRequestDto requestDto) {
+                                  @RequestBody @Valid UpdateBookRequestDto requestDto) {
         return bookService.update(id, requestDto);
     }
 
--- src/main/java/mate/academy/bookshop/dto/UpdateBookRequestDto.java
+++ src/main/java/mate/academy/bookshop/dto/UpdateBookRequestDto.java
@@ -1,13 +1,20 @@
 package mate.academy.bookshop.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class UpdateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
     private String isbn;
+    @Min(value = 0)
+    @NotNull
     private BigDecimal price;
     private String description;
     private String coverImage;
